be3d5a23327172ac4abb386a973c82dfcdadb9ee
Gal Zafar <theycallmefachfach@gmail.com>
2026-03-31 11:36:27 +0300

    Update package version to 0.2.0,  reintroduce chart.js dependency in devDependencies,
     and expose ReactorState and SimulatorState types in the public interface.

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import { stepHaberBoschReaction } from "./src/simulate"
 import { type Conditions } from "./src/simulate"
+import { type ReactorState } from "./src/reactor"
+import { type SimulatorState } from "./src/simulator"
 export {
     type Conditions,
+    type ReactorState,
+    type SimulatorState,
     stepHaberBoschReaction
 }
